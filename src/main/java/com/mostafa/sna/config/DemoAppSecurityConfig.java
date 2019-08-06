0c02e9eb92e8b1a44be3663c1ef7189c74577b9c
mostafa4099 <mostafa.sna@gmail.com>
2019-08-06 11:01:09 +0600

    first commit

---
+++ src/main/java/com/mostafa/sna/config/DemoAppSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mostafa.sna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoAppSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder builder = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication().withUser(
+				builder.username("mostafa").password("123").roles("Admin")
+			);
+		auth.inMemoryAuthentication().withUser(
+				builder.username("golam").password("123").roles("Manager")
+			);
+		auth.inMemoryAuthentication().withUser(
+				builder.username("rahman").password("123").roles("Employee")
+			);
+	}
+	
+}
